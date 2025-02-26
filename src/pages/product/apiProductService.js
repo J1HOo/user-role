b438d6565fee95880ebe874f89e7cf475b31d540
J1HOo <businessj1ho@gmail.com>
2025-02-26 10:10:50 +0900

    commit

--- src/pages/product/apiProductService.js
+++ src/pages/product/apiProductService.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api/products";
+const API_PRODUCT_URL = "http://localhost:8080/api/products";
 
-const ApiProductService = {
+const apiProductService = {
     // 상품 검색 결과 가져오기
     getProducts: function(keyword, callback, errorCallback) {
         axios
-            .get(`${API_BASE_URL}/search?keyword=${encodeURIComponent(keyword)}`)
+            .get(`${API_PRODUCT_URL}/search?keyword=${encodeURIComponent(keyword)}`) // 글씨 깨짐 방지를 위해 인코딩
             .then(
                 (res) => {
                     callback(res.data);
@@ -23,11 +23,11 @@ const ApiProductService = {
     // 추천 검색어 가져오기
     getSuggestions: function(keyword, callback, errorCallback) {
         axios
-            .get(`${API_BASE_URL}/search?keyword=${encodeURIComponent(keyword)}`)
+            .get(`${API_PRODUCT_URL}/search?keyword=${encodeURIComponent(keyword)}`)
             .then(
                 (res) => {
                     // 상품명만 추출하여 반환
-                    const suggestionList = res.data?.map(product => product.productName) || [];
+                    const suggestionList = res.data?.map(product => product.productName) || []; // data가 없는 경우 빈 배열 반환
                     callback(suggestionList);
                 }
             )
@@ -42,7 +42,7 @@ const ApiProductService = {
     // 특정 상품 상세 정보 가져오기
     getProductById: function(productId, callback, errorCallback) {
         axios
-            .get(`${API_BASE_URL}/${productId}`)
+            .get(`${API_PRODUCT_URL}/${productId}`)
             .then(
                 (res) => {
                     callback(res.data);
@@ -57,4 +57,4 @@ const ApiProductService = {
     }
 };
 
-export default ApiProductService;
+export default apiProductService;
